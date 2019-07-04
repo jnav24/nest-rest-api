8e32a24bb6b6319682fcc0b6f05aea3aa16db94d
Justin Navarro <info@justinnavarro.net>
2019-07-04 01:13:11 -0400

    scaffold an endpoint to get all items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,15 @@
 import {Controller, Get, Post, Body, Put, Delete, Param} from '@nestjs/common';
-import { CreateItemDto} from './dto/create-item.dto';
+import {CreateItemDto} from './dto/create-item.dto';
+import {ItemsService} from './items.service';
+import {ItemInterface} from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) {}
+
     @Get()
-    findAll(): string {
-        return 'Get all items';
+    findAll(): ItemInterface[] {
+        return this.itemsService.findAll();
     }
 
     @Get(':id')
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ItemInterface} from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: ItemInterface[] = [
+        {
+            id: 1,
+            name: 'Item one',
+            qty: 1,
+        },
+        {
+            id: 2,
+            name: 'Item two',
+            qty: 1,
+        },
+    ];
+
+    findAll(): ItemInterface[] {
+        return this.items;
+    }
+}
